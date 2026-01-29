48b75461cc8d20a03767ab24c79a811845fb2871
patrick barry <barrypatrick@outlook.fr>
2026-01-29 15:12:58 +0100

    Initial Commit

---
+++ PianoApp/PianoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PianoAppApp.swift
+//  PianoApp
+//
+//  Created by Franck Barry on 29/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct PianoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
